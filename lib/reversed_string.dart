60fa3e2a14bbc8d5200f20f3e989f998f4448c88
Khondakar Afridi <khondakarafridi35@gmail.com>
2022-10-08 11:53:18 +0600

    Updated

--- lib/reversed_string.dart
+++ lib/reversed_string.dart
@@ -2,8 +2,7 @@ void main(List<String> args) {
   String x =
       "We say we are walking the dog, but the dog always leads. We need more werkin and less twerkin if you ask me.";
   String reverseX = "";
-  List stringAsList = x.split("").toList();
-  for (String y in stringAsList.reversed) {
+  for (String y in x.split("").toList().reversed) {
     reverseX += y;
   }
   print(reverseX);
